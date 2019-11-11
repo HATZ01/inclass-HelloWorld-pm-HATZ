bc1d5cab1bbf39ae251504e97579f48e13e38814
Noah Hatz <Noah@Noah-Hatz-MacBook.local>
2019-11-11 20:05:23 +0100

    Copy and paste Assets and ProjectSettings

---
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class AddNumbers : MonoBehaviour
+{
+    void Start()
+    {
+               int a = 3;
+
+        int b = 4;
+
+ 
+
+        Debug.Log("a = " + a);
+
+        Debug.Log("b = " + b);
+
+        Debug.Log("a + b = " + (a + b));
+
+ 
+
+        Debug.Log("add numbers a = " + a + ", b = " + b + "; a + b = " + (a + b));
+        
+    }
+
+}
